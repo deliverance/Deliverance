8c9d569a923c7034a40188097b2ecf3a069c34ab
ianb <ianb@fd0d7bf2-dfb6-0310-8d31-b7ecfe96aada>
2006-07-31 19:14:20 +0000

    Moving files around (just moving, not editing yet)
    git-svn-id: http://codespeak.net/svn/z3/deliverance/branches/packaged@30823 fd0d7bf2-dfb6-0310-8d31-b7ecfe96aada

---
+++ setup.py
@@ -0,0 +1,27 @@
+__version__ = '0.1'
+
+from setuptools import setup, find_packages
+
+setup(name="Deliverance",
+      version=__version__,
+      description="",
+      long_description="""\
+""",
+      classifiers=[
+        # dev status, license, HTTP categories
+        ],
+      keywords='',
+      author="Paul Everitt",
+      author_email="",
+      url="",
+      license="",
+      packages=find_packages(exclude=[]),
+      zip_safe=False,
+      install_requires=[
+        'lxml',
+      ],
+      entry_points="""
+      """,
+      )
+
+
