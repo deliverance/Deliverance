7673508b6eaa227714162e2d69004ad115a10f8e
ianb <ianb@fd0d7bf2-dfb6-0310-8d31-b7ecfe96aada>
2006-08-14 17:34:11 +0000

    lxml now uses a special parser setup to have custom element class lookups; invoke that (but allow for the old implicit registration too), and remove workarounds
    git-svn-id: http://codespeak.net/svn/z3/deliverance/branches/packaged@31302 fd0d7bf2-dfb6-0310-8d31-b7ecfe96aada

--- deliverance/main.py
+++ deliverance/main.py
@@ -1,8 +1,11 @@
 import os
 from lxml import etree
 from time import time
-from lxml.etree import Namespace, ElementBase
-
+from lxml.etree import Namespace, ElementBase, XMLParser
+try:
+    from lxml.etree import ElementNamespaceClassLookup
+except ImportError:
+    ElementNamespaceClassLookup = None
 
 nsmap = {
     "dv": "http://www.plone.org/deliverance",
@@ -10,6 +13,14 @@ nsmap = {
     "xsl": "http://www.w3.org/1999/XSL/Transform",
     "at": "http://plone.org/archetypes",
     }
+
+parser = XMLParser()
+if ElementNamespaceClassLookup is not None:
+    # Earlier versions of lxml did class lookup in all
+    # cases; newer versions require this explicit parser
+    # setup
+    lookup = ElementNamespaceClassLookup()
+    parser.setElementClassLookup(lookup)
 
 class AppMap:
 
@@ -19,7 +30,8 @@ class AppMap:
         self.module_dir = os.path.dirname(os.path.abspath(__file__))
         self.layout_dir = os.path.join(self.module_dir, layout_dir)
         layoutsfn = os.path.join(self.layout_dir, "appmap.xml")
-        self.tree = etree.ElementTree(file=layoutsfn)
+        self.tree = etree.ElementTree(
+            file=layoutsfn, parser=parser)
         self.tree.xinclude()
 
         # Make a themeprocessor to style all outgoing pages.  Note that the 
@@ -27,7 +39,8 @@ class AppMap:
         # defined via a custom Python class defined below (class LayoutElement)
         root = self.tree.getroot()
         layout = root.xpath("dv:layouts/dv:layout", nsmap)[0]
-        self.themeprocessor = make_processor(layout)
+        #self.themeprocessor = make_processor(layout)
+        self.themeprocessor = layout.processor
 
 
     def publish(self, xmlstring):
@@ -44,64 +57,55 @@ class AppMap:
 class DVRuleBase(ElementBase):
 
     def getThemeNode(self):
-        return getThemeNode(self)
+        """Get a node in the theme doc"""
 
-def getThemeNode(el):
-    """Get a node in the theme doc"""
+        # Current node is a rule, get xpath from the @theme attr
+        themedoc = self.xpath("../../dv:theme", nsmap)[0][0]
+        xpath = self.get("theme")
+        try:
+            themenode = themedoc.xpath(xpath, nsmap)[0]
+        except IndexError:
+            msg = "Themedoc has no node at: %s" % xpath
+            print msg
+            themenode = None
 
-    # Current node is a rule, get xpath from the @theme attr
-    themedoc = el.xpath("../../dv:theme", nsmap)[0][0]
-    xpath = el.get("theme")
-    try:
-        themenode = themedoc.xpath(xpath, nsmap)[0]
-    except IndexError:
-        msg = "Themedoc has no node at: %s" % xpath
-        print msg
-        themenode = None
-
-    return themenode
+        return themenode
 
 
 class LayoutElement(ElementBase):
 
     def processor(self):
-        return make_processor(self)
+        """Make XSLT processor by changing theme based on rules"""
+
+        # Apply all the rules
+        for rule in self.xpath("./dv:rules/*", nsmap):
+            rule.apply()
+
+        # Merge applied rules into compilerdoc
+        compilerroot = self.xpath("../dv:compiler/xsl:stylesheet", nsmap)[0]
+        themeroot = self.xpath("dv:theme/html:html", nsmap)[0]
+        target = compilerroot.xpath("xsl:template[@match='/']", nsmap)[0]
+        target.append(themeroot)
+
+        #print etree.tostring(compilerroot)
+
+        return etree.XSLT(compilerroot)
     
     processor = property(processor)
-
-def make_processor(el):
-    """Make XSLT processor by changing theme based on rules"""
-
-    # Apply all the rules
-    for rule in el.xpath("./dv:rules/*", nsmap):
-        apply_rules(rule)
-
-    # Merge applied rules into compilerdoc
-    compilerroot = el.xpath("../dv:compiler/xsl:stylesheet", nsmap)[0]
-    themeroot = el.xpath("dv:theme/html:html", nsmap)[0]
-    target = compilerroot.xpath("xsl:template[@match='/']", nsmap)[0]
-    target.append(themeroot)
-
-    #print etree.tostring(compilerroot)
-
-    return etree.XSLT(compilerroot)
         
 
 class RuleReplaceElement(DVRuleBase):
 
     def apply(self):
-        return apply_rules(self)
-
-def apply_rules(el):
-    # TODO: Someething here
-    themenode = getThemeNode(el)
-    if themenode is None:
-        return
-    del(themenode[:])
-    themenode.text = None
-    xslvalueof = etree.SubElement(themenode,
-                                  "{%s}value-of" % nsmap["xsl"])
-    xslvalueof.set("select", el.get("content"))
+        # TODO: Someething here
+        themenode = self.getThemeNode()
+        if themenode is None:
+            return
+        del(themenode[:])
+        themenode.text = None
+        xslvalueof = etree.SubElement(themenode,
+                                      "{%s}value-of" % nsmap["xsl"])
+        xslvalueof.set("select", self.get("content"))
 
 
 class RuleCopyElement(DVRuleBase):
