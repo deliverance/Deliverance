a67dbd7e042263b5a15694861913d092f6716520
ianb <ianb@fd0d7bf2-dfb6-0310-8d31-b7ecfe96aada>
2007-03-19 17:09:40 +0000

    Added a better / more complete entry point for setting up the proxy
    git-svn-id: http://codespeak.net/svn/z3/deliverance/trunk@40784 fd0d7bf2-dfb6-0310-8d31-b7ecfe96aada

--- deliverance/proxyapp.py
+++ deliverance/proxyapp.py
@@ -3,7 +3,12 @@ WSGI proxy application that applies a deliverance theme while
 passing the request to another HTTP server
 """
 
+import os
+import urlparse
 from paste.proxy import TransparentProxy
+from paste.urlmap import URLMap
+from paste.urlparser import StaticURLParser
+from paste.exceptions import errormiddleware
 from deliverance.wsgimiddleware import DeliveranceMiddleware
 from deliverance.relocateresponse import RelocateMiddleware
 
@@ -53,6 +58,25 @@ class ProxyDeliveranceApp(object):
             environ, start_response)
     
 
+class ProxyMountedDeliveranceApp(ProxyDeliveranceApp):
+
+    def __init__(self, *args, **kw):
+        try:
+            mount_points = kw.pop('mount_points')
+        except KeyError:
+            mount_points = {}
+        self.mount_points = mount_points
+        ProxyDeliveranceApp.__init__(self, *args, **kw)
+
+    def make_app(self):
+        normal_app = ProxyDeliveranceApp.make_app(self)
+        from paste.urlmap import URLMap
+        urlmap = URLMap()
+        for name, value in self.mount_points.items():
+            urlmap[name] = value
+        urlmap['/'] = normal_app
+        return urlmap
+
 class DebugHeaders(object):
 
     translate_keys = {'CONTENT_LENGTH': 'HTTP_CONTENT_LENGTH',
@@ -77,3 +101,55 @@ class DebugHeaders(object):
                 print '  %s: %s' % (name.title(), value)
             start_response(status, headers, exc_info)
         return self.app(environ, repl_start_response)
+
+
+def make_proxy(global_conf,
+               wrap_href, theme_uri, rule_uri,
+               renderer='py', transparent=False, debug_headers=False,
+               relocate_content=False,
+               merge_cache_control=False,
+               **kw):
+    from paste.deploy.converters import asbool
+    mount_points = {}
+    for name, value in kw.items():
+        if name.startswith('mount '):
+            path = name[len('mount '):].strip()
+            if not path:
+                raise ValueError('Bad path: %r (in %r)' % (path, name))
+            mount_points[path] = StaticURLParser(os.path.abspath(value))
+        else:
+            raise ValueError(
+                "Unexpected configuration key: %r" % name)
+    if not wrap_href.startswith('http:') or wrap_href.startswith('https:'):
+        wrap_href = 'http://' + wrap_href.lstrip('/')
+    parts = urlparse.urlsplit(wrap_href)
+    scheme, netloc, path, query, fragment = parts
+    scheme = scheme.lower()
+    if scheme not in ['http', 'https']:
+        raise ValueError(
+            "I don't know how to proxy to the scheme %r (from wrap_href=%s)"
+            % (scheme, wrap_href))
+    if fragment:
+        raise ValueError(
+            "You cannot use a fragment (%r) in wrap_href=%s"
+            % (fragment, wrap_href))
+    if query:
+        raise ValueError(
+            "You cannot use a query string ?%s (from wrap_href=%s)"
+            % (query, wrap_href))
+    if path and path != '/':
+        raise ValueError(
+            "Proxying to a path on a server is not currently supported "
+            "(path=%r from wrap_href=%s)"
+            % (path, wrap_href))
+    app = ProxyMountedDeliveranceApp(
+        theme_uri=theme_uri,
+        rule_uri=rule_uri,
+        proxy=netloc,
+        transparent=asbool(transparent),
+        debug_headers=asbool(debug_headers),
+        relocate_content=asbool(relocate_content),
+        renderer=renderer,
+        mount_points=mount_points)
+    app = errormiddleware.make_error_middleware(app, global_conf)
+    return app
