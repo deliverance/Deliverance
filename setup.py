40d72af6d6e7b4ae1da45dd01dae65d4d489ef85
ianb <ianb@fd0d7bf2-dfb6-0310-8d31-b7ecfe96aada>
2008-06-30 18:10:31 +0000

    test setup.py setup
    git-svn-id: http://codespeak.net/svn/z3/deliverance/sandbox/ianb/deliverance/trunk@56189 fd0d7bf2-dfb6-0310-8d31-b7ecfe96aada

--- setup.py
+++ setup.py
@@ -26,6 +26,8 @@ transformation.
       packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
       include_package_data=True,
       zip_safe=False,
+      test_suite='nose.collector', 
+      tests_require=['nose'],
       install_requires=[
         "lxml",
       ],
