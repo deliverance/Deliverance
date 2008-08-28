9f4821d48d0c2eabfc5ed42ccd7b7d430438087f
ianb <ianb@fd0d7bf2-dfb6-0310-8d31-b7ecfe96aada>
2008-08-28 18:14:56 +0000

    Make the serialization per-thread instead of a single XSLT transform for all threads
    git-svn-id: http://codespeak.net/svn/z3/deliverance/trunk@57669 fd0d7bf2-dfb6-0310-8d31-b7ecfe96aada

--- deliverance/htmlserialize.py
+++ deliverance/htmlserialize.py
@@ -1,5 +1,6 @@
 from lxml import etree
 import re
+import threading
 
 html_xsl = """
 <xsl:transform xmlns:xsl="http://www.w3.org/1999/XSL/Transform">
@@ -13,10 +14,22 @@ html_xsl = """
 # TODO: this should be xsl for real formatting 
 pretty_html_xsl = html_xsl
 
-html_transform = etree.XSLT(etree.XML(html_xsl))
-pretty_html_transform = etree.XSLT(etree.XML(pretty_html_xsl))
+pretty_html_transforms = threading.local()
+html_transforms = threading.local()
 
+def get_pretty_html_transform():
+    try:
+        return pretty_html_transforms.transform
+    except AttributeError:
+        t = pretty_html_transforms.transform = etree.XSLT(etree.XML(pretty_html_xsl))
+        return t
 
+def get_html_transform():
+    try:
+        return html_transforms.transform
+    except AttributeError:
+        t = html_transforms.transform = etree.XSLT(etree.XML(html_xsl))
+        return t
 
 def tostring(doc, pretty = False, doctype_pair=None):
     """
@@ -27,9 +40,9 @@ def tostring(doc, pretty = False, doctype_pair=None):
     """
 
     if pretty:
-        doc = str(pretty_html_transform(doc))
+        doc = str(get_pretty_html_transform()(doc))
     else:
-        doc = str(html_transform(doc))
+        doc = str(get_html_transform()(doc))
 
     if doctype_pair: 
         doc = """<!DOCTYPE html PUBLIC "%s" "%s">\n%s""" % (doctype_pair[0], doctype_pair[1], doc) 
