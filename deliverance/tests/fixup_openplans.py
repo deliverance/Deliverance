eac8702291eec1c7d8474f47e64430e2a59cfa24
ianb <ianb@fd0d7bf2-dfb6-0310-8d31-b7ecfe96aada>
2008-09-30 05:21:39 +0000

    update the example proxy stuff for the new https layout
    git-svn-id: http://codespeak.net/svn/z3/deliverance/sandbox/ianb/deliverance/trunk@58490 fd0d7bf2-dfb6-0310-8d31-b7ecfe96aada

--- deliverance/tests/fixup_openplans.py
+++ deliverance/tests/fixup_openplans.py
@@ -1,6 +1,12 @@
+import urlparse
+import posixpath
+
 def fixup_openplans_response(request, response, orig_base, proxied_base, proxied_url, log):
-    user_base = 'http://www.openplans.org/people'
+    user_base = 'https://www.openplans.org/people'
     if response.location and response.location.startswith(user_base + '/'):
+        print 'incoming: %s (%r)' % (response.location, request.application_url)
         user_rest = response.location[len(user_base):]
-        response.location = '%s/people%s' % (request.application_url, user_rest)
+        location = posixpath.join('/people', user_rest.lstrip('/'))
+        response.location = urlparse.urljoin(request.application_url, location)
+        print 'outgoing: %s' % response.location, [location, user_rest]
 
