737964697366cc3771b99fa3060567621e4ec845
ejucovy <ejucovy@fd0d7bf2-dfb6-0310-8d31-b7ecfe96aada>
2010-10-02 13:07:01 +0000

    fix #52: broken import in tests/example.py; also, extend it to allow port/host declaration from command-line arguments
    git-svn-id: http://codespeak.net/svn/z3/deliverance/trunk@77549 fd0d7bf2-dfb6-0310-8d31-b7ecfe96aada

--- deliverance/tests/example.py
+++ deliverance/tests/example.py
@@ -3,18 +3,27 @@ This sets up an example site
 """
 
 import os
+import sys
 from paste.urlparser import StaticURLParser
 from paste.httpserver import serve
 from weberror.evalexception import EvalException
-from deliverance.middleware import DeliveranceMiddleware, RulesetGetter
+from deliverance.middleware import DeliveranceMiddleware, FileRuleGetter
 from deliverance.security import SecurityContext
 
 base_path = os.path.join(os.path.dirname(__file__), 'example-files')
 app = StaticURLParser(base_path)
-deliv_app = DeliveranceMiddleware(app, RulesetGetter('/rules.xml'))
+rules_file = os.path.join(base_path, 'rules.xml')
+deliv_app = DeliveranceMiddleware(app, FileRuleGetter(rules_file))
 full_app = SecurityContext.middleware(deliv_app, execute_pyref=True, display_logging=True, display_local_files=True,
                                       force_dev_auth=True)
 
 if __name__ == '__main__':
-    print 'See http://127.0.0.1:8080/?deliv_log for the page with log messages'
-    serve(EvalException(full_app))
+    try:
+        port = sys.argv[1]
+    except IndexError:
+        port = '8080'
+    host = '127.0.0.1'
+    if ':' in port:
+        host, port = port.split(':')
+    print 'See http://%s:%s/?deliv_log for the page with log messages' % (host, port)
+    serve(EvalException(full_app), port=port, host=host)
